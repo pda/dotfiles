eda8f1fc4c7608f3d46882201860c2b8585b667e
Paul Annesley <paul@annesley.cc>
2013-10-28 20:11:03 -0700

    install.sh, very lazy for the moment.

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln -s dotfiles/ackrc .ackrc
+ln -s dotfiles/gemrc .gemrc
+ln -s dotfiles/gitconfig .gitconfig
+ln -s dotfiles/gitignore .gitignore
+ln -s dotfiles/irbrc .irbrc
+ln -s dotfiles/slate .slate
+ln -s dotfiles/tmux.conf .tmux.conf
