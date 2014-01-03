4dd681ab13238135820a3c7da1c4facd34bce203
Paul Annesley <paul@annesley.cc>
2014-01-02 17:46:09 -0800

    Delete unused slate config. i.e. I don't use slate.

--- install.sh
+++ install.sh
@@ -5,5 +5,4 @@ ln -s dotfiles/gemrc .gemrc
 ln -s dotfiles/gitconfig .gitconfig
 ln -s dotfiles/gitignore .gitignore
 ln -s dotfiles/irbrc .irbrc
-ln -s dotfiles/slate .slate
 ln -s dotfiles/tmux.conf .tmux.conf
