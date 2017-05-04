81667bd03a882fd1c3246c09431c7ec4e42022fe
Paul Annesley <paul@annesley.cc>
2017-05-04 12:00:11 +1000

    updates before wiping paulbookpro

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-ln -s dotfiles/ackrc .ackrc
-ln -s dotfiles/gemrc .gemrc
-ln -s dotfiles/gitconfig .gitconfig
-ln -s dotfiles/gitignore .gitignore
-ln -s dotfiles/irbrc .irbrc
-ln -s dotfiles/tmux.conf .tmux.conf
+ln -s .dotfiles/gemrc .gemrc
+ln -s .dotfiles/gitconfig .gitconfig
+ln -s .dotfiles/gitignore .gitignore
+ln -s .dotfiles/irbrc .irbrc
+ln -s .dotfiles/tmux.conf .tmux.conf
