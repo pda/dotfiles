8f0f57a5c916314dc8c9126d79e18597f4ca3888
Paul Annesley <paul@annesley.cc>
2024-01-14 18:21:45 +1030

    fix ripgreprc symlink target

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ mkdir -p ~/.config/git
 ln -snf ../../.dotfiles/gitconfig ~/.config/git/config
 ln -snf ../../.dotfiles/gitignore ~/.config/git/ignore
 
-ln -snf ../../.dotfiles/ripgreprc ~/.config/ripgreprc
+ln -snf ../.dotfiles/ripgreprc ~/.config/ripgreprc
 
 mkdir -p ~/.config/tmux
 ln -snf ../../.dotfiles/tmux.conf ~/.config/tmux/tmux.conf
